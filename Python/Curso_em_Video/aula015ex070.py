41da00661402d7db034104b93937635734101150
ihenriquebr <mhenriqueof@outlook.com>
2022-04-28 16:06:31 -0300

    ex068 e ex069
    Upgrades.

---
+++ Python/Curso_em_Video/aula015ex070.py
@@ -0,0 +1,10 @@
+# Módulos
+import emoji
+# Cores
+bgn = '\033[42m'
+cc = '\033[m'
+# Abertura
+print(emoji.emojize(f' :arrow_down_small::keycap_ten::arrow_down_small: {bgn} Loja do DezConto {cc} :arrow_down_small::keycap_ten::arrow_down_small: ',use_aliases=True))
+# Variáveis
+# Estruturas
+while true
