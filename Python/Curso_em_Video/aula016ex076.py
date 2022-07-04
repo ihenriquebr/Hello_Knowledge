4576628470827a58c833c07dc59f262ace7f124d
Henrique Oliveira <88278478+ihenriquebr@users.noreply.github.com>
2022-07-04 17:31:23 -0300

    Update

--- Python/Curso_em_Video/aula016ex076.py
+++ Python/Curso_em_Video/aula016ex076.py
@@ -5,3 +5,15 @@ byw = '\033[1;43m'
 cc = '\033[m'
 # Abertura
 print(emoji.emojize(f':radio: {byw} Produtos e Preços {cc} :credit_card:',use_aliases=True))
+# Lista
+lista = ('Lápis', 2.00,
+         'Borracha', 1.50,
+         'Caderno', 20.00,
+         'Caneta', 1.75,
+         'Mochila', 30.00)
+for pos in range(0, len(lista)):
+    if pos % 2 == 0:
+        print(f'{lista[pos]}')
+    else:
+        print(f'{lista[pos]}')
+#todo terminar frases do FOR
