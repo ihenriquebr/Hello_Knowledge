953313cde373db0a67af41bbce67585be5b6d10d
Henrique Oliveira <88278478+ihenriquebr@users.noreply.github.com>
2022-07-05 17:30:31 -0300

    Update

--- Python/Curso_em_Video/aula016ex076.py
+++ Python/Curso_em_Video/aula016ex076.py
@@ -1,10 +1,11 @@
 # Módulos
 import emoji
 # Cores
+ue = '\033[4m'
 byw = '\033[1;43m'
 cc = '\033[m'
 # Abertura
-print(emoji.emojize(f':radio: {byw} Produtos e Preços {cc} :credit_card:',use_aliases=True))
+print(emoji.emojize(f'{f":radio: {byw} Produtos e Preços {cc} :credit_card:":^63}',use_aliases=True))
 # Lista
 lista = ('Lápis', 2.00,
          'Borracha', 1.50,
@@ -13,7 +14,10 @@ lista = ('Lápis', 2.00,
          'Mochila', 30.00)
 for pos in range(0, len(lista)):
     if pos % 2 == 0:
-        print(f'{lista[pos]}')
+        # Nome
+        print(f'{lista[pos]:.<30}',end='')
     else:
-        print(f'{lista[pos]}')
-#todo terminar frases do FOR
+        # Preço
+        print(f'R$ {lista[pos]:>5.2f}')
+    # Encerramento
+print(emoji.emojize(f'{f"{ue}Aproveite os descontos!{cc} :wink:":^50}',use_aliases=True))
--- Python/Curso_em_Video/aula016ex077.py
+++ Python/Curso_em_Video/aula016ex077.py
@@ -5,3 +5,33 @@ bgy = '\033[1;47m'
 cc = '\033[m'
 # Abertura
 print(emoji.emojize(f':capital_abcd: {bgy} Palavras e Vogais {cc} :a:',use_aliases=True))
+# Tupla
+palavras = ('APRENDER', 'PROGRAMAR', 'MNB')
+# Estruturas
+
+for a in palavras:
+    print(f'A palavra {a} tem as vogais: ',end='')
+    cont = 0
+    for b in a:
+        if b == "A":
+            cont += 1
+            print("a",end=' ')
+        if b == "E":
+            cont += 1
+            print("e",end=' ')
+        if b == "I":
+            cont += 1
+            print("i",end=' ')
+        if b == "O":
+            cont += 1
+            print("o",end=' ')
+        if b == "U":
+            cont += 1
+            print("u",end=' ')
+        else:
+            continue
+    if cont == 0:
+        print('não há vogais nesta palavra.')
+    cont = 0
+    print('')
+# todo terminar as palavras
