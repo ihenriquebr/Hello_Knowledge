ea39c20c80d9cc381ca824442c1d890a2df080e5
Henrique Oliveira <88278478+ihenriquebr@users.noreply.github.com>
2022-07-12 17:28:25 -0300

    Update

--- Python/Curso_em_Video/aula017ex078.py
+++ Python/Curso_em_Video/aula017ex078.py
@@ -7,20 +7,21 @@ cc = '\033[m'
 print(emoji.emojize(f'{bbe} Maior {cc} :arrow_up:   {bbe} Menor {cc} :arrow_down:   {bbe} Posição {cc} :on:',use_aliases=True))
 # Estruturas
 nums = []
-pos = 0
+maipos = []
+contmai = 0
+menpos = []
+contmen = 0
 for rep in range(0, 5):
     nums.append(int(input(f'Valor da posição {rep}: ')))
 print(nums)
-maior = 0
-pm = 0
 for p, n in enumerate(nums):
     print(p, n)
-    # Maior
-    if n > maior:
-        maior = n
-        pm = p
-    else:
-        maior = maior
-        pm = pm
-print(f'Posição: {pm} Maior: {maior}')
-# todo terminar menor
+    if n == max(nums):
+        maipos.append(p)
+        contmai += 1
+    if n == min(nums):
+        menpos.append(p)
+        contmen += 1
+print(f'O maior valor é o {max(nums)} {"na posição" if contmai == 1 else "nas posições"} {maipos}.\nO menor valor é o {min(nums)} {"na posição" if contmen == 1 else "nas posições"} {menpos}.')
+# Encerramento
+print(emoji.emojize(f'Você descobriu os maiores e menores valores com suas respectivas posições. :wink:',use_aliases=True))
